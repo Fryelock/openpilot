1a6880fa1527e1bcf55802e5e4c799a9bc72e055
Martin Lillepuu <martin@mlp.ee>
2020-04-18 23:07:42 +0300

    update preglobal checksum

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -1,41 +1,32 @@
 import copy
 from cereal import car
+from selfdrive.car.subaru.values import CAR
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
-def create_steering_control(packer, apply_steer, frame, steer_step):
+def subaru_preglobal_checksum(packer, values, addr):
+  dat = packer.make_can_msg(addr, 0, values)[2]
+  return (sum(dat[:7])) % 256
 
-  # counts from 0 to 15 then back to 0 + 16 for enable bit
-  idx = ((frame // steer_step) % 16)
+def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_step):
 
-  values = {
-    "Counter": idx,
-    "LKAS_Output": apply_steer,
-    "LKAS_Request": 1 if apply_steer != 0 else 0,
-    "SET_1": 1
-  }
-
-  elif car_fingerprint in (CAR.OUTBACK, CAR.LEGACY):
-
-    if apply_steer != 0:
-      chksm_steer = apply_steer * -1
-      chksm_engage = 1
-    else:
-      chksm_steer = 0
-      chksm_engage = 0
-
-    #counts from 0 to 7 then back to 0
-    idx = (frame / steer_step) % 8
-    steer2 = (chksm_steer >> 8) & 0x1F
-    steer1 =  chksm_steer - (steer2 << 8)
-    checksum = (idx + steer2 + steer1 + chksm_engage) % 256
-
+  if car_fingerprint in (CAR.IMPREZA):
+    # counts from 0 to 15 then back to 0 + 16 for enable bit
+    idx = ((frame // steer_step) % 16)
     values = {
       "Counter": idx,
       "LKAS_Output": apply_steer,
       "LKAS_Request": 1 if apply_steer != 0 else 0,
-      "Checksum": checksum
+      "SET_1": 1
     }
+  elif car_fingerprint in (CAR.OUTBACK, CAR.LEGACY):
+    idx = ((frame / steer_step) % 8)
+    values = {
+      "Counter": idx,
+      "LKAS_Output": apply_steer,
+      "LKAS_Request": 1 if apply_steer != 0 else 0,
+    }
+    values["Checksum"] = subaru_preglobal_checksum(packer, values, "ES_LKAS")
 
   return packer.make_can_msg("ES_LKAS", 0, values)
 
