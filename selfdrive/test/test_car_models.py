2c3996fdd37f466834a7d09db1efeadc0d48914c
Martin Lillepuu <martin@mlp.ee>
2020-05-09 19:47:35 +0300

    revert the car model test fix

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -369,7 +369,7 @@ routes = {
     'enableCamera': True,
   },
   "791340bc01ed993d|2019-03-10--16-28-08": {
-    'carFingerprint': SUBARU.CROSSTREK,
+    'carFingerprint': SUBARU.IMPREZA,
     'enableCamera': True,
   },
   "fbbfa6af821552b9|2020-03-03--08-09-43": {
@@ -431,7 +431,7 @@ non_tested_cars = [
   TOYOTA.CHRH,
   TOYOTA.HIGHLANDERH,
   TOYOTA.HIGHLANDERH_TSS2,
-  SUBARU.IMPREZA,
+  SUBARU.CROSSTREK,
 ]
 
 if __name__ == "__main__":
