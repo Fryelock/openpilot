a7f551432e8b0b49f8821de928e7e003de546412
Martin Lillepuu <martin@mlp.ee>
2020-07-09 12:25:55 +0300

    Initial merge fixes

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -114,6 +114,9 @@ typedef struct UIScene {
   int ui_viz_rw;
   int ui_viz_ro;
 
+  int lead_status;
+  float lead_d_rel, lead_y_rel, lead_v_rel;
+
   int front_box_x, front_box_y, front_box_width, front_box_height;
 
   std::string alert_text1;
@@ -141,6 +144,7 @@ typedef struct UIScene {
   cereal::ControlsState::Reader controls_state;
   cereal::DriverState::Reader driver_state;
   int gear;
+  float freeSpace;
 } UIScene;
 
 typedef struct {
